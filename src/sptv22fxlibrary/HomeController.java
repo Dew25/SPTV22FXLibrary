c476470ce3951ecc3826cdf46c7ca98be19ce25d
Dew25 <dew2@yandex.ru>
2024-03-25 10:39:48 +0200

    Реализовали функцию добавления пользователя

--- src/sptv22fxlibrary/HomeController.java
+++ src/sptv22fxlibrary/HomeController.java
@@ -5,37 +5,63 @@
  */
 package sptv22fxlibrary;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import users.newuser.NewuserController;
 
 /**
  *
  * @author admin
  */
 public class HomeController implements Initializable {
-    
-    @FXML
-    private Label label;
     private SPTV22FXLibrary app;
     
+    @FXML private Label lbHello;
+    @FXML private Label lbInfo;
+    @FXML private VBox vbContent;
+    
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private void addNewUser(){
+         try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("/users/newuser/newuser.fxml"));
+            GridPane gpNewUser = loader.load();
+            NewuserController newuserController = loader.getController();
+            newuserController.setHomeController(this);
+            app.getPrimaryStage().setTitle("SPTV22FXLibrary - Добавление нового пользователя");
+            vbContent.getChildren().clear();
+            vbContent.getChildren().add(gpNewUser);
+            
+        } catch (IOException ex) {
+            Logger.getLogger(HomeController.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        lbHello.setText("Добро пожаловать в нашу библиотеку!");
        
     }    
 
     void setApp(SPTV22FXLibrary app) {
         this.app = app;
+    }
+
+    public SPTV22FXLibrary getApp() {
+        return app;
+    }
+
+    public Label getLbInfo() {
+        return lbInfo;
     }
     
 }
--- src/users/newuser/NewuserController.java
+++ src/users/newuser/NewuserController.java
@@ -5,9 +5,17 @@
  */
 package users.newuser;
 
+import entity.Reader;
+import entity.User;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javax.persistence.EntityManager;
+import sptv22fxlibrary.HomeController;
+import tools.PassEncrypt;
 
 /**
  * FXML Controller class
@@ -15,13 +23,55 @@ import javafx.fxml.Initializable;
  * @author admin
  */
 public class NewuserController implements Initializable {
-
+    
+    private HomeController homeController;
+    
+    @FXML private Button addNewUser;
+    @FXML private TextField tfFirstName;
+    @FXML private TextField tfLastName;
+    @FXML private TextField tfPhone;
+    @FXML private TextField tfLogin;
+    @FXML private TextField tfPassword;
+    
     /**
      * Initializes the controller class.
      */
+    
+    @FXML private void clickAddNewUser(){
+        Reader reader = new Reader();
+        reader.setFirstname(tfFirstName.getText());
+        reader.setLastname(tfLastName.getText());
+        reader.setPhone(tfPhone.getText());
+        User user = new User();
+        user.setLogin(tfLogin.getText());
+        PassEncrypt pe = new PassEncrypt();
+        user.setPassword(pe.getEncryptPassword(tfPassword.getText(),pe.getSalt()));
+        user.setReader(reader);
+        user.getRoles().add(sptv22fxlibrary.SPTV22FXLibrary.ROLES.USER.toString());
+        try {
+            homeController.getApp().getEntityManager().getTransaction().begin();
+            homeController.getApp().getEntityManager().persist(reader);
+            homeController.getApp().getEntityManager().persist(user);
+            homeController.getApp().getEntityManager().getTransaction().commit();
+            homeController.getLbInfo().setText("Пользователь успешно добавлен");
+            tfFirstName.setText("");
+            tfLastName.setText("");
+            tfPhone.setText("");
+            tfLogin.setText("");
+            tfPassword.setText("");
+        } catch (Exception e) {
+            homeController.getLbInfo().setText("Добавить пользователя не удалось");
+        }
+    }
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    
+
+    public void setHomeController(HomeController homeController) {
+        this.homeController = homeController;
+    }
     
 }
